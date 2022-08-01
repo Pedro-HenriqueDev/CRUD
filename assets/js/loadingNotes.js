3f6acf413e3fc6ef5d83c983118bb3839d9a73e5
Pedro Henrique <82292364+Pedro-HenriqueDev@users.noreply.github.com>
2022-08-01 12:17:53 -0300

    bug fix

--- assets/js/loadingNotes.js
+++ assets/js/loadingNotes.js
@@ -1,8 +1,6 @@
 function loadingNotes() {
     btnAdd.addEventListener('click', registerNote)
-    if(localStorage.getItem(1) == null) {
-        list.innerHTML = 'Suas notas apareceram aqui!'
-    }
+    
     let lenghNotes = localStorage.getItem('id')
     for(let i = 1;i <= lenghNotes; i++) {
         let item = JSON.parse(localStorage.getItem(i))
